def7bad56273745b9c0e4e5fe06d25b98a5ef22f
SpencerMuniz <sbottomley44@gmail.com>
2021-10-06 10:19:13 -0700

    fixing spelling error

--- herd.py
+++ herd.py
@@ -1,8 +1,8 @@
 from dinosaur import Dinosaur
 from weapon import Weapon
-class Heard():
+class Herd():
     def __init__(self):
         self.dinosaurs = [Dinosaur("Slattern", 30000, 30000, Weapon("Triple-Crowned Tail Whip", 5000)), Dinosaur("Raiju", 20000, 10000, Weapon("Armored Headbutt", 1000)), Dinosaur("Otachi", 15000, 20000, Weapon("Acid Spray", 2000)), Dinosaur("Scunner", 20000, 1000, Weapon("Armor pplated four arm claws", 1500))]
 
-    def create_heard(self):
+    def create_herd(self):
         pass
